7ae847eff4157cccb310dbf14925383caf7aa20c
Fiamma Belen Coedo <coedo.fiamma@gmail.com>
2022-08-04 20:53:54 -0300

    changes

--- src/interpreters/addSubstractInterpreter.py
+++ src/interpreters/addSubstractInterpreter.py
@@ -4,23 +4,24 @@ npl = spacy.load('es_core_news_lg') # TODO: Ver de hacerlo mas global
 
 def translate(statement):
     operators = {"suma": "+", "resta": "-"} # Aca deberia ir la palabra raiz nomas
-    doc = npl(statement) # Agregar u?
+    doc = npl(statement)
     mathProblem = []
     for token in doc:
         if token.pos_ in ["NOUN", "NUM"]:
             mathProblem.append(token)
-            # print(f"{token.text:{10}} {token.pos_:{10}} {token.is_stop:{10}} {spacy.explain(token.tag_)}")
+        print(f"{token.text:{10}} {token.pos_:{10}} {token.is_stop:{10}} {spacy.explain(token.tag_)}")
     # print(problema)
 
     def translate(token):
-      if token.pos_ == "NOUN":
+      if token.text in list(operators.keys()): # TODO: Agregar logica lemmatizar y abstraer (se repite abajo)
         return (operators[token.text], token)
-      else: return (token.text, token)
+      else:
+        return (token.text, token)
     translatedProblem = list(map(translate, mathProblem))
     # print(problema_traducido)
     finalTranslatedProblem = []
     for palabra, token in translatedProblem:
-      if token.pos_ == "NOUN":
+      if token.text in list(operators.keys()):
         noun = palabra
       else:
         finalTranslatedProblem.append(palabra)
